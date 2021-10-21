6cd698effef92b3a5342515720c2837bda785cfc
Oleh Fedorenko <ofedoren@redhat.com>
2021-10-21 12:58:35 +0000

    Add raw message for Ansible

--- webpack/src/Router/HostReports/ShowPage/Components/ReportLogs/Ansible.js
+++ webpack/src/Router/HostReports/ShowPage/Components/ReportLogs/Ansible.js
@@ -1,16 +1,35 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { Alert, AlertActionCloseButton } from '@patternfly/react-core';
+import { Alert, AlertActionCloseButton, Button } from '@patternfly/react-core';
 
 import { translate as __ } from 'foremanReact/common/I18n';
+import { useForemanModal } from 'foremanReact/components/ForemanModal/ForemanModalHooks';
+
+import RawMsgModal from './Components/RawMsgModal';
 
 import { msgLevelClasses } from './helpers';
 
+import { RAW_MSG_MODAL_ID } from '../../../constants';
+
 const AnsibleLogs = ({ logs, checkMode }) => {
   const [alertVisibility, setAlertVisibility] = useState(true);
+  const [selectedMsg, setSelectedMsg] = useState(0);
+  const { setModalOpen: setRawModalOpen } = useForemanModal({
+    id: RAW_MSG_MODAL_ID,
+  });
+
+  const rawMsg = idx => {
+    const onClick = () => {
+      setSelectedMsg(idx);
+      setRawModalOpen();
+    };
+    return <Button onClick={onClick} variant="secondary">{__('Show')}</Button>;
+  };
+
   return (
     <>
+      <RawMsgModal body={logs[selectedMsg]} />
       {checkMode && alertVisibility ? (
         <Alert
           variant="info"
@@ -31,7 +50,8 @@ const AnsibleLogs = ({ logs, checkMode }) => {
           <tr>
             <th className="col col-md"> {__('Level')} </th>
             <th className="col col-md-3"> {__('Task')} </th>
-            <th className="col col-md-9"> {__('Message')} </th>
+            <th className="col col-md-8"> {__('Message')} </th>
+            <th className="col col-md-1"> {__('Raw data')} </th>
           </tr>
         </thead>
         <tbody>
@@ -52,11 +72,12 @@ const AnsibleLogs = ({ logs, checkMode }) => {
               ) : (
                 <td className="break-me">{log.friendlyMessage}</td>
               )}
+            <td className="break-me">{rawMsg(idx)}</td>
             </tr>
           ))}
           {logs.length === 0 ? (
             <tr key="tr-0">
-              <td colSpan="3">{__('Nothing to show')}</td>
+              <td colSpan="4">{__('Nothing to show')}</td>
             </tr>
           ) : null}
         </tbody>
---
+++ webpack/src/Router/HostReports/ShowPage/Components/ReportLogs/Components/RawMsgModal.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import JSONTree from 'react-json-tree';
+import Immutable from 'seamless-immutable';
+
+import { translate as __ } from 'foremanReact/common/I18n';
+import ForemanModal from 'foremanReact/components/ForemanModal';
+
+import { RAW_MSG_MODAL_ID } from '../../../../constants';
+
+const RawMsgModal = ({ body }) => {
+  const theme = {
+    scheme: 'foreman',
+    backgroundColor: 'rgba(0, 0, 0, 255)',
+    base00: 'rgba(0, 0, 0, 0)',
+  };
+  return (
+    <ForemanModal
+      id={RAW_MSG_MODAL_ID}
+      title={__('Raw logs')}
+      backdrop="static"
+      enforceFocus
+    >
+      <JSONTree
+        data={Immutable.asMutable(body, { deep: true })}
+        hideRoot
+        theme={theme}
+      />
+    </ForemanModal>
+  );
+};
+
+RawMsgModal.propTypes = {
+  body: PropTypes.object,
+};
+
+RawMsgModal.defaultProps = {
+  body: {},
+};
+
+export default RawMsgModal;
